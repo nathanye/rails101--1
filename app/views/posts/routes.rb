944a5dacdaf26380a5d28b8d67d1e021986f609e
nathanye <khalil54781@gmail.com>
2018-08-20 21:01:06 +0800

    implement post create function

---
+++ app/views/posts/routes.rb
@@ -0,0 +1,37 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+
+  namespace :api, :defaults => { :format => :json } do
+    namespace :v1 do
+      get "/reservations" => "reservations#index", :as => :reservations
+      get "/trains" => "trains#index", :as => :trains
+      get "/trains/:train_number" => "trains#show", :as => :train
+
+      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
+      post "/reservations" => "reservations#create", :as => :create_reservations
+      patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
+      delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "logout" => "auth#logout"
+
+      get "/me" => "users#show", :as => :user
+      patch "/me" => "users#update", :as => :update_user
+    end
+  end
+
+
+
+  resources :cities do
+    member do
+      post :update_temp
+    end
+  end
+
+
+  root 'welcome#index'
+
+end
